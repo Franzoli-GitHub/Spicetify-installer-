ef0c894b88edf767d0e74c7bf174971ccbdfe1f2
Franzoli-GitHub <86996244+Franzoli-GitHub@users.noreply.github.com>
2024-06-14 14:15:17 +0000

    Update spicetify.cmd

--- spicetify.cmd
+++ spicetify.cmd
@@ -1,10 +1,9 @@
 @echo off
-echo For install spicetify INPUT(1)
-echo For Fix spicetify INPUT(2)
-set /p userInput= INPUT: 
-if "%userInput%"== "1" (
+set /p userInput= Do you have spicetify?(CASE SENSITIVE) (Yes/No):
+if "%userInput%"== "Yes" (
      echo Self fix Mode
-) else if "%userInput%"== "2" (
+     Taskkill /F /IM spotify.exe
+) else if "%userInput%"== "No" (
     color 01
     echo Installing spicetify!
     echo ██████  ██    ██      ██████  ██████  ██      ██ ███████ ██       ██████  ██     ██ ███████ ██████  
@@ -12,29 +11,32 @@ if "%userInput%"== "1" (
     echo ██████    ████       ██      ██    ██ ██      ██ █████   ██      ██    ██ ██  █  ██ █████   ██████  
     echo ██   ██    ██        ██      ██    ██ ██      ██ ██      ██      ██    ██ ██ ███ ██ ██      ██   ██ 
     echo ██████     ██         ██████  ██████  ███████ ██ ██      ███████  ██████   ███ ███  ███████ ██   ██
-    timeout /t 2 /nobreak
-    powershell -command "iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex"
-    powershell -command "iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex"
+    timeout /t 5 /nobreak
+    Echo Y | powershell -command "iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex"
+    powershell -command "iwr -useb https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.ps1 | iex"
+    Start C:\Users\Franzoli\AppData\Roaming\Spotify\Spotify.exe
     echo Done installing! 
    echo Completed Self Cleaning Now!
-   timeout /t 2 /nobreak
+   timeout /t 6 /nobreak
    Taskkill /F /IM Spotify.exe
+   Taskkill /F /IM spotify.exe
+   timeout /t 6 /nobreak
+   Start C:\Users\Franzoli\AppData\Roaming\Spotify\Spotify.exe
    Taskkill /F /IM cmd.exe
 ) else (
  echo invalid command
- timeout /t 1 /nobreak
- pause
+ timeout /t 5 /nobreak
  exit
 )
 pause
-color 01
+color 2
 echo ██████  ██    ██      ██████  ██████  ██      ██ ███████ ██       ██████  ██     ██ ███████ ██████  
 echo ██   ██  ██  ██      ██      ██    ██ ██      ██ ██      ██      ██    ██ ██     ██ ██      ██   ██ 
 echo ██████    ████       ██      ██    ██ ██      ██ █████   ██      ██    ██ ██  █  ██ █████   ██████  
 echo ██   ██    ██        ██      ██    ██ ██      ██ ██      ██      ██    ██ ██ ███ ██ ██      ██   ██ 
 echo ██████     ██         ██████  ██████  ███████ ██ ██      ███████  ██████   ███ ███  ███████ ██   ██
-echo This will will fix spicetify
-timeout /t 2 /nobreak                                                                                                                                                                                                                                                                                                                                                                                                             
+echo This will update and use a backup of spicetify 
+timeout /t 5 /nobreak                                                                                                                                                                                                                                                                                                                                                                                                             
 powershell -command spicetify upgrade
 powershell -command spicetify backup
 powershell -command spicetify backup apply
@@ -42,6 +44,6 @@ powershell -command spicetify restore backup apply
 Taskkill /F /IM Spotify.exe
 Taskkill /F /IM spotify.exe
 cls
-timeout /t 2 /nobreak
+echo Completed Self Cleaning Now!
+timeout /t 5 /nobreak
 Taskkill /F /IM cmd.exe
-
